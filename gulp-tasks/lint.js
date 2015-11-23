728637da5be544b27da8a2d81ba2fdcd5cc5b6b4
Jordon de Hoog <jordon.dehoog@gmail.com>
2015-11-23 18:36:45 -0500

    Fixed the output for the linter
    Added in some needed front end packages
    BOWER BE GONE! #newtrend

--- gulp-tasks/lint.js
+++ gulp-tasks/lint.js
@@ -7,26 +7,29 @@ var help = conf.help;
 
 var $ = require('gulp-load-plugins')();
 
-gulp.task('vet:server', help.vet.server, function (){
-  return lintFiles('Server', conf.ts.server);
+var listServer = false;
+gulp.task('vet:server', help.vet.server, function () {
+  var lint = lintFiles('Server', conf.ts.server, listServer);
+  listServer = true;
+  return lint;
 });
 
+var listClient = false;
 gulp.task('vet:client', help.vet.client, function () {
-  return lintFiles('Client', conf.ts.client);
+  var lint = lintFiles('Client', conf.ts.client, listClient);
+  listClient = true;
+  return lint;
 });
 
 gulp.task('vet', help.vet.both, ['vet:server', 'vet:client']);
 
 var listFiles = false;
-function lintFiles(title, files) {
-  var lint = gulp.src(files)
+function lintFiles(title, files, display) {
+  return gulp.src(files)
     .pipe($.plumber({ errorHandler: conf.errorHandler }))
       .pipe($.cached('vet:' + title))
         .pipe($.tslint())
         .pipe($.tslint.report('prose', { summarizeFailureOutput: true }))
-        .pipe($.size({ title: 'Linter:' + title, showFiles: listFiles }))
+        .pipe($.size({ title: 'Linter:' + title, showFiles: display }))
     .pipe($.plumber.stop());
-
-  listFiles = true;
-  return lint;
 }
--- gulp-tasks/watch.js
+++ gulp-tasks/watch.js
@@ -8,4 +8,5 @@ var $ = require('gulp-load-plugins')();
 
 gulp.task('watch', conf.help.watch, ['build'], function () {
   gulp.watch(conf.ts.server, ['build:server']);
+  gulp.watch(conf.ts.client, ['vet:client']); // temporary until i get build process done
 });

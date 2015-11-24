da899c63b5b62afbf907c6ae6d67cd5785947274
Jordon de Hoog <jordon.dehoog@gmail.com>
2015-11-24 14:32:45 -0500

    Added webpack config, fixed some client stuff

--- src/client/app/home/home.module.ts
+++ src/client/app/home/home.module.ts
@@ -14,6 +14,6 @@ let mod =
   angular
     .module('app.home', dependencies)
     .config(routes)
-    .directive(homeDirective);
+    .directive('home', homeDirective);
 
 export default mod.name;
---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+var path = require('path');
+var webpack = require('webpack');
+var HtmlWebpackPlugin = require('html-webpack-plugin');
+
+var entryTs = path.resolve('./src/client/index.ts');
+var entryOut = path.resolve('./build/client');
+
+module.exports = {
+  entry: entryTs,
+  output: {
+    path: entryOut,
+    filename: 'bundle-[hash:6].js'
+  },
+  module: {
+    loaders: [{
+      test: /\.html$/,
+      loader: 'file?name=templates/[name]-[hash:6].html'
+    }, {
+      test: /\.css$/,
+      loader: "style!css"
+    }, {
+      test: /\.scss$/,
+      loader: 'style-loader!css-loader!autoprefixer-loader!sass-loader'
+    }, {
+      test: /\.ts$/,
+      exclude: /(node_modules)/,
+      loader: "ng-annotate?add=false!babel!ts-loader"
+    }, {
+      test: [/fontawesome-webfont\.svg/, /fontawesome-webfont\.eot/, /fontawesome-webfont\.ttf/, /fontawesome-webfont\.woff/, /fontawesome-webfont\.woff2/],
+      loader: 'file?name=fonts/[name].[ext]'
+    }, {
+      test: /\.png$/,
+      loader: 'url-loader?limit=100000&mimetype=image/png'
+    }, {
+      test: /\.jpg$/,
+      loader: 'file-loader'
+    }]
+  },
+  plugins: [
+    new HtmlWebpackPlugin()
+  ],
+  resolve: {
+    modulesDirectories: [
+      'node_modules'
+    ],
+    extensions: ['', '.webpack.js', '.ts', '.js']
+  }
+};

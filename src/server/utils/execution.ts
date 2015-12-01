f10deb70365f54ebcfa07463a477c5324fc2156c
Jordon de Hoog <jordon.dehoog@gmail.com>
2015-11-30 21:18:30 -0500

    Tweaked the timer a little bit

--- src/server/utils/execution.ts
+++ src/server/utils/execution.ts
@@ -1,18 +1,33 @@
 'use strict';
 
 /**
+ * Default amount of decimal places for milliseconds
+ */
+const DEFAULT_PERCISION = 3;
+
+/**
  * Class to calculate the execution time of a block
  * of code.  Gets start time on `new` instance, or can
  * be reset with {this.reset()}.
  */
 export class ExecutionTimer {
   private _start: number[];
+  private _percision: number;
 
   /**
-   * Start the timer
+   * Start the timer and set output percision
+   * @param {number}  percision (optional) Number of decimal places to output
    */
-  constructor() {
+  constructor(percision?: number) {
     this._start = process.hrtime();
+    this._percision = percision || DEFAULT_PERCISION;
+  }
+
+  /**
+   * Set a new percision
+   */
+  set percision(percision: number) {
+    this._percision = percision;
   }
 
   /**
@@ -41,9 +56,10 @@ export class ExecutionTimer {
     let seconds = end[0];
     let milliseconds = end[1] / 1000000;
 
+    let formatted = seconds + 's ';
     if (percise) {
-      return seconds + 's ' + milliseconds + 'ms';
+      return formatted + milliseconds + 'ms';
     }
-    return seconds + 's ' + milliseconds.toFixed(3) + 'ms';
+    return formatted + milliseconds.toFixed(this._percision) + 'ms';
   }
 }
